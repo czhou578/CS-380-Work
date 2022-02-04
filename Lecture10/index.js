d487eb2126aa7590298f7344fc6cc2e8bd795a76
czhou578 <colizu2020@gmail.com>
2022-02-03 17:59:15 -0800

    lecture 2/3/2022

---
+++ Lecture10/index.js
@@ -0,0 +1,10 @@
+// const fetch = require('node-fetch')
+const { add } = require('./myModule')
+
+// console.log(fetch);
+
+// fetch('https://www.google.com')
+//   .then(response => response.text())
+//   .then(text => console.log(text))
+
+console.log(add(1,2));
