72da38402606080545aaef62200e57718be2c315
czhou578 <colizu2020@gmail.com>
2022-03-08 10:26:45 -0800

    finished participants

--- Homework5/routes/participants.js
+++ Homework5/routes/participants.js
@@ -4,7 +4,44 @@ const database = require('../database')
 const router = express.Router()
 
 router.get('/get-participants', (req, res) => {
-  let sql = `SELECT * FROM `
+  let sql = `SELECT * FROM participants`
+  database.query(sql, function (error, result) {
+    if (error) throw error
+
+    res.send(result)
+  })
 })
+
+router.post('/add-participant', (req, res) => {
+  const { groupId, userId } = req.body
+
+  if (groupId && userId) {
+    let sql = `INSERT INTO participants (group_id, participant_id) VALUES (${groupId}, ${userId})`
+    database.query(sql, function (error, result) {
+      if (error) throw error
+  
+      res.send('participant added')
+    })
+  } else {
+    res.send('invalid parameters, try again.')
+  }
+})
+
+router.delete('/delete-participant', (req, res) => {
+  const { groupId, userId } = req.body
+
+  if (groupId && userId) {
+    let sql = `DELETE FROM participants WHERE group_id = ${groupId} AND participant_id = ${userId}`
+    database.query(sql, function (error, result) {
+      if (error) throw error
+
+      res.send('participant deleted')
+    })
+  } else {
+    res.send('deletion failed, try again')
+  }
+})
+
+
 
 module.exports = router
